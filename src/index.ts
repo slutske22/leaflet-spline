34c7135477ad5d3537eec129e4ddadb928be783c
Seth Lutske <sethlutske@Seths-MBP.lan>
2022-02-08 12:41:34 -0800

    Working on control points

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import * as L from "leaflet";
 import "leaflet-curve";
 
+type Tuple = [number, number];
+
 export class Spline extends L.Polyline {
-  _polyline: L.Polyline;
-  _points: [number, number][] = [];
+  _points: Tuple[] = [];
 
   constructor(path: L.LatLngExpression[], options: L.PathOptions = {}) {
     super(path, options);
-    this._polyline = L.polyline(path, options);
     this.transformPoints(path);
   }
 
@@ -17,9 +17,9 @@ export class Spline extends L.Polyline {
   transformPoints(path: L.LatLngExpression[]) {
     if (Array.isArray(path[0]) && path[0].length === 2) {
       // If path given is array of array of numbers, it is already in proper format
-      this._points = path as [number, number][];
+      this._points = path as Tuple[];
     } else if ((path[0] as L.LatLng | L.LatLngLiteral).lat) {
-      // If path is given as latlngs, transform into [number, number] format for
+      // If path is given as latlngs, transform into Tuple / [number, number] format for
       this._points = (path as (L.LatLng | L.LatLngLiteral)[]).map((latlng) => [
         latlng.lat,
         latlng.lng,
@@ -27,7 +27,48 @@ export class Spline extends L.Polyline {
     }
   }
 
-  drawBezier() {}
+  drawBezier() {
+    const points: Tuple[] = [...this._points];
+    const first: Tuple = [...points[0]];
+    const second: Tuple = [...points[1]];
+
+    /** Whether or not the path given is a closed shape - last point must be same as first */
+    const isClosedShape =
+      points[0][0] === points[points.length - 1][0] &&
+      points[0][1] === points[points.length - 1][1];
+
+    if (isClosedShape) {
+      points.pop();
+    }
+
+    /**
+     * Group points into threes - 1rst and 3rd used to create control point, second corresponds to control
+     * point to be used that gets created by 1rst and 3rd
+     */
+    const triads: [Tuple, Tuple, Tuple][] = [];
+    while (points.length > 2) {
+      const triad: [Tuple, Tuple, Tuple] = [points[0], points[1], points[2]];
+      triads.push(triad);
+      points.shift();
+    }
+    if (isClosedShape) {
+      triads.push([points[0], points[1], first]);
+      points.unshift();
+      triads.push([points[0], first, second]);
+    }
+
+    /** Control points to be used in creating the bezier curve */
+    const controlPoints = triads
+      .map((triad) => triad.map((tuple) => L.latLng(tuple)))
+      .map((triad) => {
+        const [p0, p1, p2] = triad;
+        const cpLat = p1.lat * 2 - (p0.lat + p2.lat) / 2;
+        const cpLng = p1.lng * 2 - (p0.lng + p2.lng) / 2;
+        return L.latLng({ lat: cpLat, lng: cpLng });
+      });
+
+    controlPoints.forEach((point) => L.circleMarker(point).addTo(this._map));
+  }
 
   addTo(map: L.Map | L.LayerGroup<any>): this {
     map.addLayer(this);
